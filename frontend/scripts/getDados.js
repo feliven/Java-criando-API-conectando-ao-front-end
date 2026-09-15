f4dced8fbef91ea030a7b6df15ae6258cd592848
FVS-MBA <feliven@gmail.com>
2026-09-15 20:01:28 -0300

    ajustes para deploy

--- frontend/scripts/getDados.js
+++ frontend/scripts/getDados.js
@@ -1,10 +1,11 @@
 // URL base da API
-const baseURL = "http://localhost:8080";
+// const baseURL = "http://localhost:8080";
+const baseURL = "/api";
 
 export default function getDados(endpoint) {
   return fetch(`${baseURL}${endpoint}`)
     .then((response) => response.json())
     .catch((error) => {
-      console.error("Erro ao acessar o endpoint /series/top5:", error);
+      console.error(`Erro ao acessar o endpoint ${endpoint}:`, error);
     });
 }
